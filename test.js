12238bc709c66c9df422b812cc2ae5248f26c8c7
Matty Maloney <mattymaloney@gmail.com>
2015-02-26 22:01:32 +0900

    upgraded test.js to contain tests individually for each challenge script

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
-var test = require("./03-basic-map");
+var scripts = {
+  '03': function basicMap () {
+    var test = require("./03-basic-map");
+    console.log('output: ', test([1, 3, 5, 7]));
+  },
+  '04': function basicFilter () {
+    var test = require("./04-basic-filter");
+    console.log('output', test([
+      { message: "qwre qwer qwre qwre qwre" },
+      { message: "asdf asdf asdf asdf asdf" },
+      { message: "zxcv zxcv zxcv zcxv zxcv" },
+      { message: "qwre qwer" },
+      { message: "asdf asdf" },
+      { message: "zxcv zxcv" }
+    ]))
+  }
+}
 
-console.log('output: ', test([1, 3, 5, 7]));
+
+scripts[process.argv[2]]();
+
